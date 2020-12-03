11cf31cbbfec6fe024e1e25209206c10d0f07a1e
Jorge Guimarães Martins <jorgeguimaraesdev@gmail.com>
2020-12-03 17:45:43 -0400

    Update calculator.py
    ajudando mais ainda na interpretação de quem ta executando e de quem esta produzindo este script python.

--- calculator.py
+++ calculator.py
@@ -3,7 +3,8 @@ import time
 import math
 #imports
 
-#progress_bar
+#defs
+    #loading
 def progress_bar(done):
     print("\rCarregando: [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100),end='')
 
@@ -11,7 +12,8 @@ def test():
     for n in range(101):
         progress_bar(n/100)
         time.sleep(0.009)
-#progress_bar
+    #loading
+#defs
 
 #print-calculos
 print('-'*50)
@@ -23,9 +25,9 @@ print('[ 5 ] Raiz Quadrada')
 print('-'*50)
 #print-calculos
 
-#pergunta
-sr1 = int(input(print('Qual conta você quer fazer?: ')))
-#pergunta
+#pergunta-calculo
+sr1 = int(input(print('Qual calculo você vai querer efetuar?: ')))
+#pergunta-calculo
 
 #soma-2num
 if sr1 == 1:
